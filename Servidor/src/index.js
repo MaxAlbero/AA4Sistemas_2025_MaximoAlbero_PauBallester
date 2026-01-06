cb6b187626fcfe26457b147446b8e73efb48b304
MaquinaVirtualClase <maximo.albero@estudiant.enti.cat>
2026-01-06 16:46:01 +0100

    added initial API call - need to modify the code to the updated version

--- Servidor/src/index.js
+++ Servidor/src/index.js
@@ -1,1 +1,59 @@
-console.log("Hello World");
+//Init
+const express = require("express");
+
+app = express(); //Aixo crida al constructor i el posa a la variable "app" 
+// (26/11: sense el const, "app" es una variable global, només podem fer això aqui, de normal esta malament)
+
+// require("./bddSetup");
+
+//Settings Section
+app.set("port", process.env.PORT || 3000); //la variable por la estic posant perque la vull fer servir després
+app.set("json spaces", 2); //
+
+//Middlewares
+
+const morgan = require("morgan");
+//aixo ens permet dir-li a una variable utilitzar una clase 
+app.use(morgan("dev"));
+//app.use(morgan("combined"));
+
+//Express url work setup
+app.use(express.urlencoded({extended: false})); //aixo fa que es treballi amb coses mes senzilles
+app.use(express.json()); //aixo ens permet indicar que express pot treballar amb json
+
+app.get("/", (req,res) => {
+    res.json({"Title": "HelloWorld"});
+});
+//app.use(require("./routes/_routes"));
+
+app.listen(app.get("port"), () => {
+    const ip = GetIp();
+    const port = app.get("port");
+
+    console.log("Servidor en la url: http://" + ip + ":" + port + "/");
+});
+
+//Función de ayuda para obtener la IP - por ahora se queda comentada
+
+const { networkInterfaces} = require ("os");
+GetIp = () => {
+    const nets = networkInterfaces();
+
+    const results = {};
+
+    for(const name of Object.keys(nets)){
+        for(const net of nets[name]){
+            const ipv4Family = typeof net.family == "string" ? "IPv4" : 4
+            
+            if(net.family === ipv4Family && !net.internal) {
+                if(!results[name]){
+                    results[name] = [];
+                }
+                results[name].push(net.address);
+            }
+        }
+    }
+
+    //return results;
+    return results["enp0s3"][0]; //La IP
+}
---
+++ Servidor/src/routes/_routes.js
@@ -0,0 +1,15 @@
+const { Router } = require("express");
+const router = Router();
+
+router.get("/", (req, res) => { //quan algu façi una petició get. Que en aquest cas es demanar alguna cosa a traves de la url
+
+    // res.send("Hello World");
+    res.json({Title: "Hello World"});
+});
+
+router.use("/characters", require("./characters"));
+router.use("/weapons", require("./weapons"));
+
+router.use("/chat", require("./chat"));
+
+module.exports = router; //quan algu fa un require d'aquest arxiu, em retorna el que hi ha dins de module.exports
